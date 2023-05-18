a815af25c12724dd2a35ff78416d43ab68e3a89d
Filip <filipelblag@gmail.com>
2023-05-18 10:34:39 +0200

    display notifications

--- core/consumers.py
+++ core/consumers.py
@@ -1,5 +1,6 @@
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from django.template.loader import get_template
 
 
 class NotificationConsumer(WebsocketConsumer):
@@ -15,4 +16,8 @@ class NotificationConsumer(WebsocketConsumer):
         async_to_sync(self.channel_layer.group_discard)(self.GROUP_NAME, self.channel_name)
 
     def user_joined(self, event):
+        html = get_template("core/partials/notification.html").render(
+            context={"username": event["text"]}
+        )
         self.send(text_data=event["text"])
+        self.send(text_data=html)
